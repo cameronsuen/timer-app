b366e09e9bc1c80ee7b7d2ad71148a6e2b81690f
Cameron Suen <cameron10913@gmail.com>
2017-04-05 19:02:29 +0800

    Fixed erroneous imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { MomentModule } from 'angular2-moment';
-
 import { AppComponent }  from './app.component';
 import { TaskComponent } from './task.component';
 
 @NgModule({
-  imports:      [ BrowserModule, MomentModule ],
+  imports:      [ BrowserModule],
   declarations: [ AppComponent, TaskComponent ],
   bootstrap:    [ AppComponent ]
 })
